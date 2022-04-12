cef49f8462f919906c2dbb705198eba83001e77b
Herna Rusdiyana <hernarusdiyana704@gmail.com>
2022-04-12 15:14:55 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tugas 3 Herna Rusdiyana</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="js/bootstrap.js">
+</head>
+<body>
+    <div class="container">
+    <header>
+        <div class="row">
+            <div class="col-4">
+                <div class="hero-images">
+                    <img src="images/pp.png" class="img-fluid" width="200" alt="Profile Photo">
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="hero-profiles">
+                    <h1>heynaaa.exe</h1>
+
+                    <span class="posts">
+                        <b>8</b> Post
+                    </span>
+                    <span class="followers">
+                        <a href="#"><b>223</b> Followers</a>
+                    </span>
+                    <span class="following">
+                        <a href="#"><b>500</b> Following</a>
+                    </span>
+
+                    <h4>Herna Rusdiyana</h4>
+                    <span class="description">
+                        Just do it. <br>
+                        Nothing is Impossible.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <div class="row g-1 g-lg-4">
+        <div class="feed col-4">
+            <a href="images/feed-1.jpg">
+                <img src="images/feed-1.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-2.jpg">
+                <img src="images/feed-2.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-3.jpg">
+                <img src="images/feed-3.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-4.jpg">
+                <img src="images/feed-4.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-5.jpg">
+                <img src="images/feed-5.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-6.jpg">
+                <img src="images/feed-6.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-7.jpg">
+                <img src="images/feed-7.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-8.jpg">
+                <img src="images/feed-8.jpg" class="img-fluid">
+            </a>
+        </div>
+        <div class="feed col-4">
+            <a href="images/feed-9.jpg">
+                <img src="images/feed-9.jpg" class="img-fluid">
+            </a>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
